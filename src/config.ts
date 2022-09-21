0f192a841f9a72f1895415c9a530f93c0fa5e2e8
VanesStalkers <ivan.mironov@bk.ru>
2022-09-21 20:43:31 +0300

    * process.env

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,29 @@ import * as redis from '../config/storage/redis/config.json';
 import * as greensms from '../config/service/sms/greensms.json';
 
 export default {
-  pg: pg.development,
-  redis: redis,
-  greensms: greensms,
+  pg: process.env.PG_HOST
+    ? {
+        host: process.env.PG_HOST,
+        username: process.env.PG_USER,
+        password: process.env.PG_PASS,
+        database: 'calendar',
+        dialect: 'postgres',
+      }
+    : pg.development,
+  redis: process.env.REDIS_HOST
+    ? {
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT,
+      }
+    : redis,
+  greensms: process.env.GREENSMS_URL
+    ? {
+        url: process.env.GREENSMS_URL,
+        account: {
+          user: process.env.GREENSMS_USER,
+          pass: process.env.GREENSMS_PASS,
+          from: 'Wazzup',
+        },
+      }
+    : greensms,
 };
